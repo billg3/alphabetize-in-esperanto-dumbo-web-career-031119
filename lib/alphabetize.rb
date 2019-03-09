ea4008e0ea15408558a6672066efda5c8afb9584
Bill G <gbilly3@gmail.com>
2019-03-09 19:21:01 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
 def alphabetize(arr)
   # code here
+  
+  eequire "pry"
+def alphabetize(arr)
+  # code here
+  ea = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz" #ESPERANTO_ALPHABET
+  arr.sort_by do |word|
+    word.split('').map do |letter|
+      ea.index(letter)
+    end
+  end
+
 end
